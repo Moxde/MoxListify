4496545ddcbe94fac4694885a3a99eefb4a38dc4
Moxde <m.mohamadmurad@yahoo.com>
2025-03-08 00:11:23 +0100

    `Added Itembox component and related styles for displaying ingredient input`

--- App/Screens/Recips.js
+++ App/Screens/Recips.js
@@ -1,13 +1,14 @@
 import React,{useState,useRef} from 'react'
 import { View,StyleSheet, Text,TouchableOpacity,Image,TextInput, ScrollView } from 'react-native';
 import Colors from '../constant/Colos'
-
+import { Picker } from '@react-native-picker/picker';
 
 function Recips (){
+    const [showItemBox, setShowItemBox] = useState(false);
     return(
         <View style={style.mainCont}>
             <RecipesAndIngredientDisplay/>
-
+            {showItemBox && <Itembox />}
         </View>
     )
 }
@@ -58,7 +59,7 @@ function RecipesAndIngredientDisplay() {
                     <Image source={require('../assets/img/x1btn.png')} style={style.btn} />
                 </TouchableOpacity>
             ) : (
-                <TouchableOpacity style={style.btncont} onPress={handleActivate}>
+                <TouchableOpacity style={style.btncontg} onPress={handleActivate}>
                     <Image source={require('../assets/img/addbtn.png')} style={style.btn} />
                 </TouchableOpacity>
             )}
@@ -81,8 +82,44 @@ function RecipesAndIngredientDisplay() {
       </View>
     );
   }
-  
 
+function Itembox () {
+    return (
+        <View style={style.itembox}>
+
+            <View style={style.itemboxcont}>
+                <TouchableOpacity style={style.closeContBtn}>
+                    <Text style={style.closeContText}>X</Text>
+                     
+                </TouchableOpacity>
+                <View style={style.itemboxcontInput}>
+                    <TextInput
+                        style={style.itemboxname}
+                        placeholder="Zutat eintragen:"
+                        placeholderTextColor={Colors.whitedarl}
+                    />
+                    <View style={style.inputUnitContainer}>
+                        <Picker
+                        
+                        style={style.inputUnit}
+                        
+                        >
+                            <Picker.Item label="KG" value="KG" />
+                            <Picker.Item label="G" value="G" />
+                            <Picker.Item label="EL" value="EL" />
+                            <Picker.Item label="St." value="St" />
+                        </Picker>
+                    </View>
+                </View>
+                <View style={style.seph}/> 
+                <TouchableOpacity style={style.itemboxaddbtn}>
+                    <Text style={style.itemboxaddtext}>Hinzufügen</Text>
+                </TouchableOpacity>
+            </View>
+            
+        </View>
+    )    
+}
 const style = StyleSheet.create ({
 
     mainCont:{
@@ -128,13 +165,22 @@ const style = StyleSheet.create ({
         alignItems:"center",
         backgroundColor:Colors.reddark
     },
+    btncontg:{
+        height:40,
+        width:40,
+        left:"88%",
+        top:-29,
+        borderWidth:2,
+        borderRadius:10,
+        borderColor:Colors.greencheck,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:Colors.greencheck
+    },
     btn:{
         width:30,
         height:30,
-        tintColor:Colors.whitedarl,
-        
-        
-        
+        tintColor:Colors.whitedarl, 
     },
     ingredienDisplayCont :{
         position:"absolute",
@@ -209,6 +255,98 @@ const style = StyleSheet.create ({
         height:25,
         tintColor:Colors.whitedarl,
     },
+    itembox :{
+        position:"absolute",    
+        width:"100%",
+        height:"100%",
+        backgroundColor:Colors.transpertblc,
+        alignItems:"center",
+        justifyContent:"center",
+    },
+    itemboxcont:{
+        width:"85%",
+        height:"30%",
+        backgroundColor:Colors.primary,
+        top:"-25%",
+        borderRadius:25,
+        borderColor:Colors.whitedarl,
+        borderWidth:2,
+        alignItems:"center",
+        
+       
+    },
+    itemboxcontInput:{
+        
+        marginTop:"7%",
+        flexDirection:"row",
+        justifyContent:"space-between",
+        marginHorizontal:2,
+        marginTop:50
+        
+    },
+    itemboxname:{
+        fontSize:18,
+        borderRadius:25,
+        height:60,
+        width:"50%",
+        color:Colors.textwhite,
+        borderColor:Colors.whitedarl,
+        borderWidth:2,
+        backgroundColor:Colors.primarydark,
+        paddingLeft:15,
+        marginRight:10
+    },
+    inputUnitContainer:{
+        backgroundColor:Colors.primarydark,
+        height:"100%",
+        width:"40%",
+        borderRadius:25,
+        marginHorizontal:1,
+        borderColor:Colors.whitedarl,
+        borderWidth:2,
+        
+    },
+    inputUnit:{
+        fontSize:15,
+        
+    },
+    seph:{
+        width:"95%",
+        height:2,
+        backgroundColor:Colors.whitedarl,
+        marginTop:20,
+    },
+    closeContBtn:{
+        position:"absolute",
+        left:"86%",
+        top:9,
+        backgroundColor:Colors.reddark,
+        height:30,
+        width:30,
+        borderRadius:30,
+
+    },
+    closeContText:{
+        fontSize:20,
+        textAlign:"center",
+        color:Colors.whitedarl
+    },
+    itemboxaddbtn:{
+        height:45,
+        width:150,
+        backgroundColor:Colors.greencheck,
+        borderRadius:25,
+        justifyContent:"center",
+        alignItems:"center",
+        marginTop:25
+    },
+    itemboxaddtext:{
+        fontSize:20,
+        color:Colors.textwhite,
+        textAlign:"center",
+        
+
+    }
     
 })
 
